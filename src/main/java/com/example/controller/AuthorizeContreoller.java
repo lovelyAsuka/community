9e2ceb043b63e7bd1a5192546f80667a38053114
lovelyAsuka <484519841@qq.com>
2021-08-30 22:46:05 +0800

    add check cookies

--- src/main/java/com/example/controller/AuthorizeContreoller.java
+++ src/main/java/com/example/controller/AuthorizeContreoller.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -32,7 +34,8 @@ public class AuthorizeContreoller {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request
+                           HttpServletRequest request,
+                           HttpServletResponse response
                             ){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setCode(code);
@@ -45,12 +48,14 @@ public class AuthorizeContreoller {
         String name = githubUser.getName();
         if(githubUser !=null&&name !=null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setName(githubUser.getName());
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
             //登录成功，写cookie和session
             request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
