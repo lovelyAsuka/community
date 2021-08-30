c7709d6474d57090e34ee5ab2e035f5c1188906a
lovelyAsuka <484519841@qq.com>
2021-08-30 16:20:37 +0800

    extract github

---
+++ src/main/java/com/example/controller/AuthorizeContreoller.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+
+import com.example.dto.AccessTokenDto;
+import com.example.dto.GithubUser;
+import com.example.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeContreoller {
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.uri}")
+    private String clientUri;
+    @Autowired
+    private GithubProvider githubProvider;
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setCode(code);
+        accessTokenDto.setState(state);
+        accessTokenDto.setRedirect_uri(clientUri);
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getGithubUser(accessToken);
+        String name = user.getName();
+        System.out.println(user.getName());
+        return "index";
+    }
+}
